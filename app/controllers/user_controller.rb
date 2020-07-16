f14e3eafa3feee1c22480159575b90adb9b118b4
swade781977 <swade781977@yahoo.com>
2020-07-16 14:29:27 -0400

    added delete action, added delete and edit buttons to show page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -41,11 +41,18 @@ class UserController < ApplicationController
 
     patch '/users/:id' do
         @user = User.find_by_id(params[:id])
-        binding.pry
         if @user.update(name: params[:name], email: params[:email], username: params[:username], password_digest: params[:password_digest])
             redirect "/users/#{@user.id}"
         else
             redirect "/users/#{@user.id}/edit"
         end
+    end
+
+    delete '/users/:id' do
+        user = User.find_b_id(params[:id])
+        if user
+            user.delete
+        end
+        redirect '/users'
     end
 end
