f2e9c8b3d6ffa702fcbbc2e41badebb78faa84e7
swade781977 <swade781977@yahoo.com>
2020-07-15 21:59:48 -0400

    User show made and working in basic form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,13 @@ class UserController < ApplicationController
         erb :'users/index'
     end
 
-    get 'users/:id' do
-        @user= 
+    get '/users/:id' do
+        @user = User.find_by_id(params[:id])
+        binding.pry
+        if @user
+            erb :'users/show'
+        else
+            redirect "/users"
+        end
+    end
 end
