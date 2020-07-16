03705292d86f5d6529502d77ed8ab0489e62ce59
swade781977 <swade781977@yahoo.com>
2020-07-16 00:48:26 -0400

    users/edit is working, added validation in user.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,27 +2,44 @@ class UserController < ApplicationController
     get '/users' do
         @users= User.all
 
-        erb :'users/index'
+        erb :'/users/index'
     end
 
     get '/users/new' do
-        erb :'users/new'
+        erb :'/users/new'
     end
 
     get '/users/:id' do
         @user = User.find_by_id(params[:id])
         if @user
-            erb :'users/show'
+            erb :'/users/show'
         else
             redirect "/users"
         end
     end
 
-   
+    get '/users/:id/edit' do 
+        @user = User.find_by_id(params[:id])
+        if @user
+            erb :'/users/edit'
+        else
+            redirect "/users"
+        end
+    end
 
     post '/users' do
         binding.pry
         @user = User.create(params)
         redirect "/users/#{@user.id}"
+    end
+
+    patch '/users/:id' do
+        @user = User.find_by_id(params[:id])
+        binding.pry
+        if @user.update(name: params[:name], email: params[:email], username: params[:username], password_digest: params[:password_digest])
+            redirect "/users/#{@user.id}"
+        else
+            redirect "/users/#{@user.id}/edit"
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :username, presence: true
+    validates :password_digest, presence: true
 end
