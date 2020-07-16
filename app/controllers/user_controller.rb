196fd600d93916e87848a90893576c943771798e
swade781977 <swade781977@yahoo.com>
2020-07-15 21:48:22 -0400

    users index works on basic level

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,10 @@
 class UserController < ApplicationController
+    get '/users' do
+        @users= User.all
+
+        erb :'users/index'
+    end
+
+    get 'users/:id' do
+        @user= 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+user1 = User.create(:name => "chris", :username => "chris", :email => "chris@chris.com", :password_digest => "chris")
+user2 = User.create(:name => "dena", :username => "dena", :email => "dena@dena.com", :password_digest => "dena")
+user3 = User.create(:name => "bella", :username => "bella", :email => "bella@bella.com", :password_digest => "bella")
