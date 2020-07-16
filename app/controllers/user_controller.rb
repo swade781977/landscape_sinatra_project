ccc3ca3e9371f9f76f58b4a652dcc2a1df86323d
swade781977 <swade781977@yahoo.com>
2020-07-15 23:45:28 -0400

    added users/new action and view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,13 +5,24 @@ class UserController < ApplicationController
         erb :'users/index'
     end
 
+    get '/users/new' do
+        erb :'users/new'
+    end
+
     get '/users/:id' do
         @user = User.find_by_id(params[:id])
-        binding.pry
         if @user
             erb :'users/show'
         else
             redirect "/users"
         end
+    end
+
+   
+
+    post '/users' do
+        binding.pry
+        @user = User.create(params)
+        redirect "/users/#{@user.id}"
     end
 end
